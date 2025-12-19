9726dc4f60d8faa8ed41da7cb661fb2c5117a04c
Neiber <Neiberzambran@gmail.com>
2025-12-19 12:32:23 -0500

    conexión a la base de datos e inicio del plan de estudios bootcamp SQL

---
+++ Fase1/conectar a bd.py
@@ -0,0 +1,24 @@
+# librerías
+
+import pandas as pd
+import psycopg2
+
+# conexión a la base de datos
+
+# 1. Conexión a la base de datos PostgreSQL
+
+def connect_db( ):
+    try:
+        conn = psycopg2.connect( # se conecta a la base de datos
+            host="localhost",
+            database= "pagila", # nombre de la base de datos
+            user="postgres", # usuario Conectado
+            password="123456789",   
+            port="5432"
+        )
+        print("Conexión exitosa")
+        return conn # retorna la conexión
+    except Exception as e: # manejo de errores
+        print("Error de conexión", e)
+        return None
+
